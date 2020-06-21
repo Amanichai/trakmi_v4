dc3e88c680c3f4f21062c39cb3f51be69a3bb7a2
Amanichai <amanichai@gmail.com>
2020-06-20 23:31:40 -0400

    add react-navigation basic functionality

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button } from 'react-native';
+import { NavigationActions, NavigationEvents } from 'react-navigation';
 
-const SignupScreen = ()=>{
-    return <View>
+const SignupScreen = ({ navigation })=>{
+    return <>
                 <Text style={{fontSize: 28, textAlign: 'center'}}>SignupScreen</Text>
-            </View>
+                <Button
+                    title='Go to Signin'
+                    onPress={()=> navigation.navigate('Signin')}
+                ></Button>
+                <Button
+                    title='Go to Mainflow'
+                    onPress={()=> navigation.navigate('mainFlow')}
+                ></Button>
+            </>
 }
 
 const styles = StyleSheet.create({
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = ()=>{
-    return <View>
-                <Text style={{fontSize: 28, textAlign: 'center'}}>TrackListScreen</Text>
-            </View>
+const TrackListScreen = ({ navigation })=>{
+    return <>
+                <Text style={{fontSize: 28, textAlign: 'center'}}>TrackListScreen</Text>{}
+                <Button
+                    title='Go to Track Details'
+                    onPress={()=> navigation.navigate('TrackDetails') }
+                ></Button>
+            </>
 }
 
 const styles = StyleSheet.create({
