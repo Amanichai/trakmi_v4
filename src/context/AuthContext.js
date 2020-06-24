29f90705b23da62d7e3bc1b23602481e6cf21157
Amanichai <amanichai@gmail.com>
2020-06-23 20:16:00 -0400

    add navigationRef.js and set outside navigation feature

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,31 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trakerApi from '../api/traker';
+import { navigate } from '../navigationRef';
 
 
 const authReducer = (state, action)=>{
     switch(action.type){
         case 'error_msg':
             return {...state, errorMessage: action.payload}
+        case 'signup':
+            return{errorMessage: '', token: action.payload}
         default:
             return state;
     }
 }
 
-const signup = (dispatch) =>{
-    return async ({email, password})=>{
+const signup = (dispatch) => async ({email, password})=>{
         try{
             const response = await trakerApi.post('/signup', {email, password});
-            console.log(response.data)
+            await AsyncStorage.setItem('token', response.data.token)
+            dispatch({ type: 'signup', payload: response.data.token})
+            navigate('mainFlow');
         }catch (err){
             dispatch({type: 'error_msg', payload: 'Invalid email or password'})
         }  
     }
-}
+
 
 const signin = (dispatch)=>{
     return ({email, password})=>{
@@ -37,5 +42,5 @@ const signout = (dispatch) =>{
 export const {Provider, Context} = createDataContext(
     authReducer,
     {signup, signin, signout},
-    {isSignedIn: false, errorMessage: ''}
+    {token: null, errorMessage: ''}
 )
